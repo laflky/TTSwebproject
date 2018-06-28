ede8532687de7603f74a6536a8fe883e1c0c5ba6
michelle <michellebilich@gmail.com>
2018-06-28 09:28:40 -0400

    added project to githubb

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,44 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
+
+$(document).ready(function() {
+        // Transition effect for navbar 
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 500) { 
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+        });
+});
+
+
+$(document).ready(function() {
+  //carousel options
+  $('#quote-carousel').carousel({
+    pause: true, interval: 10000,
+  });
+});
+
+
+
+
+
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'welcome#home'
+
+  get 'about' => 'welcome#about'
+
+  get 'info' => 'welcome#info'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
